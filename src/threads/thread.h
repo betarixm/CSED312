aa5e7a0e252d63fd77815addcb0b97df441dfa9c
beta <beta.underalpha@gmail.com>
2021-10-04 22:25:37 +0900

    🏗 Add thread fields for implementing donations

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -24,6 +24,9 @@ typedef int tid_t;
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
 
+/* Priority donation properties */
+#define DONATION_DEPTH_MAX 8            /* Max search depth. */
+
 /* A kernel thread or user process.
 
    Each thread structure is stored in its own 4 kB page.  The
@@ -88,6 +91,7 @@ struct thread
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
+    int init_priority;                  /* Initial priority */
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
@@ -95,6 +99,12 @@ struct thread
     
     /* Tick when thread have to wake up. */
     int64_t awake_tick;
+    
+    struct lock *released_lock;
+
+    /* List for multiple donation */
+    struct list donations;
+    struct list_elem donation_elem;
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
