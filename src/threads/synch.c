b97cf981300c64ad43ff4e3b59a4acf0d1c533e6
beta <beta.underalpha@gmail.com>
2021-10-04 16:57:16 +0900

    🔢 waiters caring about threads' priorities

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -314,7 +314,7 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
-  list_push_back (&cond->waiters, &waiter.elem);
+  list_insert_ordered (&cond->waiters, &waiter.elem, compare_sema_priority, NULL);
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
