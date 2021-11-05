2196434cc16df22b5f35e6dc144bb7bfecdade60
= <minseokk@postech.ac.kr>
2021-11-06 01:49:32 +0900

    🛠️ Bad ptr reference handled

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -148,6 +148,12 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
+  if ((user && is_kernel_vaddr (fault_addr)) 
+    || (!user && is_user_vaddr (fault_addr)))
+  {
+    sys_exit (-1);
+  }
+
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
