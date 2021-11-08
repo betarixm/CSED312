7b9a4ea975bf2568650387bd3460efaa7ab3f7cc
= <minseokk@postech.ac.kr>
2021-11-08 21:41:38 +0900

    🔥 Hotfix: Unused include deleted

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -18,8 +18,6 @@
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 
-#include "devices/timer.h"
-
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
 
