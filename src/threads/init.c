cfbd72e00b8e1a950755bcfc2683bfc27ce577a1
= <minseokk@postech.ac.kr>
2021-11-30 22:43:50 +0900

    🏁 Init frame

--- src/threads/init.c
+++ src/threads/init.c
@@ -37,6 +37,7 @@
 #include "filesys/filesys.h"
 #include "filesys/fsutil.h"
 #endif
+#include "vm/frame.h"
 
 /* Page directory with kernel mappings only. */
 uint32_t *init_page_dir;
@@ -126,6 +127,8 @@ main (void)
   locate_block_devices ();
   filesys_init (format_filesys);
 #endif
+
+  frame_init ();
 
   printf ("Boot complete.\n");
   
