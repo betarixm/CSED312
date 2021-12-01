22c5a4d7eaa7ddee5ab28de459a2bcf03dca32d8
= <minseokk@postech.ac.kr>
2021-12-01 19:39:20 +0900

    🔮 Page declared

---
+++ src/vm/page.c
@@ -0,0 +1,172 @@
+#include "vm/page.h"
+#include "threads/thread.h"
+#include "vm/frame.h"
+#include <string.h>
+
+static hash_hash_func spt_hash_func;
+static hash_less_func spt_less_func;
+static void page_destutcor (struct hash_elem *elem, void *aux);
+extern struct semaphore rw_mutex, mutex;
+
+void
+init_spt (struct hash *spt)
+{
+  hash_init (spt, spt_hash_func, spt_less_func, NULL);
+}
+
+void
+destroy_spt (struct hash *spt)
+{
+  hash_destroy (spt, page_destutcor);
+}
+
+void
+init_spte (struct hash *spt, void *upage, void *kpage)
+{
+  struct spte *e;
+  e = (struct spte *) malloc (sizeof *e);
+  
+  e->upage = upage;
+  e->kpage = kpage;
+  
+  e->status = PAGE_FRAME;
+  
+  hash_insert (spt, &e->hash_elem);
+}
+
+void
+init_zero_spte (struct hash *spt, void *upage)
+{
+  struct spte *e;
+  e = (struct spte *) malloc (sizeof *e);
+  
+  e->upage = upage;
+  e->kpage = NULL;
+  
+  e->status = PAGE_ZERO;
+  
+  e->file = NULL;
+  e->writable = true;
+  
+  hash_insert (spt, &e->hash_elem);
+}
+
+void
+init_frame_spte (struct hash *spt, void *upage, void *kpage)
+{
+  struct spte *e;
+  e = (struct spte *) malloc (sizeof *e);
+
+  e->upage = upage;
+  e->kpage = kpage;
+  
+  e->status = PAGE_FRAME;
+  
+  hash_insert (spt, &e->hash_elem);
+}
+
+struct spte *
+init_file_spte (struct list *spt, void *_upage, struct file *_file, off_t _ofs, uint32_t _read_bytes, uint32_t _zero_bytes, bool _writable)
+{
+  struct spte *e;
+  
+  e = (struct spte *)malloc (sizeof *e);
+
+  e->upage = _upage;
+  e->kpage = NULL;
+  
+  e->file = _file;
+  e->ofs = _ofs;
+  e->read_bytes = _read_bytes;
+  e->zero_bytes = _zero_bytes;
+  e->writable = _writable;
+  
+  e->status = PAGE_FILE;
+  
+  hash_insert (spt, &e->hash_elem);
+  
+  return e;
+}
+
+bool
+load_page (struct hash *spt, void *upage)
+{
+  struct spte *e;
+  uint32_t *pagedir;
+  void *kpage;
+
+  e = get_spte (spt, upage);
+  if (e == NULL)
+    sys_exit (-1);
+
+  kpage = falloc_get_page (PAL_USER, upage);
+  if (kpage == NULL)
+    sys_exit (-1);
+
+  sema_down (&rw_mutex);
+
+  if (file_read_at (e->file, kpage, e->read_bytes, e->ofs) != e->read_bytes)
+  {
+    falloc_free_page (kpage);
+    sema_up (&rw_mutex);
+    sys_exit (-1);
+  }
+  
+  memset (kpage + e->read_bytes, 0, e->zero_bytes);
+
+  sema_up (&rw_mutex);
+    
+  pagedir = thread_current ()->pagedir;
+
+  if (!pagedir_set_page (pagedir, upage, kpage, e->writable))
+  {
+    falloc_free_page (kpage);
+    sys_exit (-1);
+  }
+
+  e->kpage = kpage;
+  e->status = PAGE_FRAME;
+
+  return true;
+}
+
+struct spte *
+get_spte (struct hash *spt, void *upage)
+{
+  struct spte e;
+  struct hash_elem *elem;
+  
+  e.upage = upage;
+  elem = hash_find (spt, &e.hash_elem);
+
+  return elem != NULL ? hash_entry (elem, struct spte, hash_elem) : NULL;
+}
+
+static unsigned
+spt_hash_func (const struct hash_elem *elem, void *aux)
+{
+  struct spte *p = hash_entry(elem, struct spte, hash_elem);
+
+  return hash_bytes (&p->kpage, sizeof (p->kpage));
+}
+
+static bool 
+spt_less_func (const struct hash_elem *a, const struct hash_elem *b, void *aux)
+{
+  void *a_kpage = hash_entry (a, struct spte, hash_elem)->kpage;
+  void *b_kpage = hash_entry (b, struct spte, hash_elem)->kpage;
+
+  return a_kpage < b_kpage;
+}
+
+static void
+page_destutcor (struct hash_elem *elem, void *aux)
+{
+  struct spte *e;
+
+  e = hash_entry (elem, struct spte, hash_elem);
+
+  // TODO take care dirty
+
+  free(e);
+}
---
+++ src/vm/page.h
@@ -0,0 +1,36 @@
+#ifndef VM_PAGE_H
+#define VM_PAGE_H
+
+#include <hash.h>
+#include "filesys/file.h"
+#include "filesys/off_t.h"
+
+#define PAGE_ZERO 0
+#define PAGE_FRAME 1
+#define PAGE_FILE 2
+
+struct spte
+  {
+    void *upage;
+    void *kpage;
+  
+    struct hash_elem hash_elem;
+  
+    int status;
+  
+    struct file *file;  // File to read.
+    off_t ofs;  // File off set.
+    uint32_t read_bytes, zero_bytes;  // Bytes to read or to set to zero.
+    bool writable;  // whether the page is writable.
+  };
+
+void init_spt (struct hash *);
+void destroy_spt (struct hash *);
+void init_spte (struct hash *, void *, void *);
+void init_zero_spte (struct hash *, void *);
+void init_frame_spte (struct hash *, void *, void *);
+struct spte *init_file_spte (struct list *, void *, struct file *, off_t, uint32_t, uint32_t, bool);
+bool load_page (struct hash *, void *);
+struct spte *get_spte (struct hash *, void *);
+
+#endif
